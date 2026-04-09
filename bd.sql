6efd650c825d338ad364795f131e0c68b9b0e6f8
juandi <juanosorio1124226@correo.itm.edu.co>
2026-04-09 01:36:29 -0500

    proy

--- bd.sql
+++ bd.sql
@@ -40,5 +40,105 @@ CREATE TABLE practica_estrategia (
   descripcion NVARCHAR(45) NOT NULL
 );
 
+CREATE TABLE facultad (
+  id INT NOT NULL PRIMARY KEY,
+  nombre NVARCHAR(60) NOT NULL,
+  tipo NVARCHAR(45) NOT NULL,
+  fecha_fun DATE NOT NULL,
+  universidad INT NOT NULL,
+  CONSTRAINT fk_unidad_sede FOREIGN KEY (universidad)
+    REFERENCES universidad (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+GO
+
+-- Tabla programa
+CREATE TABLE programa (
+  id INT NOT NULL PRIMARY KEY,
+  nombre NVARCHAR(60) NOT NULL,
+  tipo NVARCHAR(45) NOT NULL,
+  nivel NVARCHAR(45) NOT NULL,
+  fecha_creacion NVARCHAR(45) NOT NULL,
+  fecha_cierre NVARCHAR(45) NULL,
+  numero_cohortes NVARCHAR(45) NOT NULL,
+  cant_graduados NVARCHAR(45) NOT NULL,
+  fecha_actualizacion NVARCHAR(45) NOT NULL,
+  ciudad NVARCHAR(45) NOT NULL,
+  facultad INT NOT NULL,
+  CONSTRAINT fk_programa_facultad FOREIGN KEY (facultad)
+    REFERENCES facultad (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+GO
+
+CREATE TABLE pasantia (
+  id INT NOT NULL PRIMARY KEY,
+  nombre NVARCHAR(45) NOT NULL,
+  pais NVARCHAR(45) NOT NULL,
+  empresa NVARCHAR(45) NOT NULL,
+  descripcion NVARCHAR(45) NOT NULL,
+  programa INT NOT NULL,
+  CONSTRAINT fk_pasantia_programa FOREIGN KEY (programa)
+    REFERENCES programa (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+GO
+CREATE TABLE acreditacion (
+  resolucion INT NOT NULL PRIMARY KEY,
+  tipo NVARCHAR(45) NOT NULL,
+  calificacion NVARCHAR(45) NOT NULL,
+  fecha_inicio NVARCHAR(45) NOT NULL,
+  fecha_fin NVARCHAR(45) NOT NULL,
+  programa INT NOT NULL,
+  CONSTRAINT fk_acreditacion_programa FOREIGN KEY (programa)
+    REFERENCES programa (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+GO
+
+CREATE TABLE registro_calificado (
+  codigo INT NOT NULL PRIMARY KEY,
+  cant_creditos NVARCHAR(45) NOT NULL,
+  hora_acom NVARCHAR(45) NOT NULL,
+  hora_ind NVARCHAR(45) NOT NULL,
+  metodologia NVARCHAR(45) NOT NULL,
+  fecha_inicio DATE NOT NULL,
+  fecha_fin DATE NOT NULL,
+  duracion_anios NVARCHAR(45) NOT NULL,
+  duracion_semestres NVARCHAR(45) NOT NULL,
+  tipo_titulacion NVARCHAR(45) NOT NULL,
+  programa INT NOT NULL,
+  CONSTRAINT fk_registro_calificado_programa FOREIGN KEY (programa)
+    REFERENCES programa (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+GO
+
+CREATE TABLE premio (
+  id INT NOT NULL PRIMARY KEY,
+  nombre NVARCHAR(45) NOT NULL,
+  descripcion NVARCHAR(45) NOT NULL,
+  fecha DATE NOT NULL,
+  entidad_otorgante NVARCHAR(45) NOT NULL,
+  pais NVARCHAR(45) NOT NULL,
+  programa INT NOT NULL,
+  CONSTRAINT fk_premio_programa FOREIGN KEY (programa)
+    REFERENCES programa (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+GO
+
+CREATE TABLE programa_ci (
+  programa INT NOT NULL,
+  car_innovacion INT NOT NULL,
+  PRIMARY KEY (programa, car_innovacion),
+  CONSTRAINT fk_programa_ci_programa FOREIGN KEY (programa)
+    REFERENCES programa (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT fk_programa_ci_car_innovacion FOREIGN KEY (car_innovacion)
+    REFERENCES car_innovacion (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+GO
 insert into enfoque values(1, 'no se', ' no se ')
 select * from car_innovacion
